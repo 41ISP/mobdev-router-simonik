dabbdbbecdcbbaf8a76f084eda9ebe2ee0eb025a
Nikalasha888 <veronikasimonik702@gmail.com>
2026-09-16 07:27:08 +0000

    1

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Header from "./Header"
+
+const  Layout = () => {
+    return (
+        <>
+        <Header />
+        <main className = "page-area"> 
+          <Outlet />  
+        </main>
+        </>
+    )
+}
+
+export default Layout 
